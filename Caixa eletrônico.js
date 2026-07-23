a9eac08ef56e0df981cb443f1c20b0c80a87e9c4
LevizBlessedpo <marcosmcod@gmail.com>
2026-07-23 15:21:50 -0300

    integrei uma biblioteca readline-sync e criei uma CLI no caixa

--- Caixa eletrônico.js
+++ Caixa eletrônico.js
@@ -1,31 +1,32 @@
+import readline from 'readline-sync';
 let Saldo = 1000;
 let Opcao = 0
 let ArmazenarValor = 0;
 let SacarValor = 0;
 
-while (Opcao !== 4) 
+while (Opcao !== '4') 
 {
-  Opcao = +prompt("(1) - Saldo Atual, (2) -  Depositar valor, (3) - sacar, (4) - sair: ");
+  Opcao = readline.question('(1) - Saldo Atual, (2) -  Depositar valor, (3) - sacar, (4) - sair: ');
   
   switch (Opcao) 
      {
-  case 1:
-    alert("Atualmente você possui " + Saldo + " Reais.");
+  case '1':
+    console.log("Atualmente você possui " + Saldo + " Reais.");
     break;
-  case 2:
-   ArmazenarValor = +prompt("Digite um valor para depositar: ");
+  case '2':
+   ArmazenarValor = readline.questionFloat("Digite um valor para depositar: ");
    Saldo = Saldo + ArmazenarValor;
-   alert("O valor depositado foi de: " + ArmazenarValor + " Reais" + " O valor total após o deposito foi de: " + Saldo + " Reais");
+   console.log("O valor depositado foi de: " + ArmazenarValor + " Reais" + " O valor total após o deposito foi de: " + Saldo + " Reais");
     break;
-  case 3:
-   SacarValor = +prompt("Digite  valor para sacar: ");
+  case '3':
+   SacarValor = readline.questionFloat("Digite  valor para sacar: ");
    if(SacarValor <= 0 || SacarValor > Saldo) 
   { 
-    alert("Errado, digite um valor correto.");
+    console.log("Errado, digite um valor correto.");
   } else 
     {
      Saldo = Saldo - SacarValor;
-     alert("O valor sacado foi: " + SacarValor + " Reais" + " O valor após o saque é de: " + Saldo + " Reais");
+     console.log("O valor sacado foi: " + SacarValor + " Reais" + " O valor após o saque é de: " + Saldo + " Reais");
     }
     break;
      }
